fd8d9be489e5995232a26dd842d3ed4547c37e47
AnanyaCherukuri05 <ananyachrukuri5@gmail.com>
2025-04-23 10:35:41 +0530

    ..

--- MenuItem.js
+++ MenuItem.js
@@ -1,17 +1,9 @@
 const mongoose = require('mongoose');
 
 const menuItemSchema = new mongoose.Schema({
-  name: { 
-    type: String, 
-    required: true
-   },
-  description:{
-    type: String
-  },
-  price: { 
-    type: Number, 
-    required: true 
-  }
+  name: { type: String, required: true },
+  description: String,
+  price: { type: Number, required: true }
 });
 
 const MenuItem = mongoose.model('MenuItem', menuItemSchema);
